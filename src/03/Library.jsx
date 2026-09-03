fab16014974d4e92f4da17f67cacf47707030d30
hak801 <bvnyonghag@gmail.com>
2026-09-03 11:20:55 +0900

    .

--- src/03/Library.jsx
+++ src/03/Library.jsx
@@ -4,9 +4,11 @@ import Book from "./Book";
 function Library() {
     return(
         <div>
-            <Book name = "처음 만난 Java" numOfPage={700}/>
-            <Book name = "완전 독학 파이썬" numOfPage={200}/>
-            <Book name = "난생 처음 C++" numOfPage={600}/>
+            <Book name = "처음 만난 Java" numOfPage={700} imgUrl = "https://www.hanbit.co.kr/_next/image?url=https%3A%2F%2Fcdn-prod.hanbit.co.kr%2Fbooks%2F0d5bc993-3eb4-4c78-a5d4-11b4a80c9af4.png&w=512&q=100"/>
+            <Book name = "한달 완성 파이썬" numOfPage={200} imgUrl = "https://www.hanbit.co.kr/_next/image?url=https%3A%2F%2Fcdn-prod.hanbit.co.kr%2Fbooks%2F0d5bc993-3eb4-4c78-a5d4-11b4a80c9af4.png&w=512&q=100"/>
+            <Book name = "재밌다 HTML" numOfPage={300} imgUrl = "https://www.hanbit.co.kr/_next/image?url=https%3A%2F%2Fcdn-prod.hanbit.co.kr%2Fbooks%2F0d5bc993-3eb4-4c78-a5d4-11b4a80c9af4.png&w=512&q=100"/>
+            <Book name = "처음 만난 C" numOfPage={1020} imgUrl = "https://www.hanbit.co.kr/_next/image?url=https%3A%2F%2Fcdn-prod.hanbit.co.kr%2Fbooks%2F0d5bc993-3eb4-4c78-a5d4-11b4a80c9af4.png&w=512&q=100"/>
+            <Book name = "처음 만난 Swift" numOfPage={490} imgUrl = "https://www.hanbit.co.kr/_next/image?url=https%3A%2F%2Fcdn-prod.hanbit.co.kr%2Fbooks%2F0d5bc993-3eb4-4c78-a5d4-11b4a80c9af4.png&w=512&q=100"/>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import './index.css';
 // import App from './App';
 // import TodoListApp from "./01/TodoListApp";
 import reportWebVitals from './reportWebVitals';
-import Library from "./03/Library";
-import './03/Book.css'
+// import Library from "./03/Enhanced_CSS/Library";
+// import './03/Enhanced_CSS/Book.css'
+ import Clock from "./04/Clock";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <Library />
-    </React.StrictMode>
-);
 
+setInterval(() => {
+    root.render(
+        <React.StrictMode>
+            <Clock/>
+        </React.StrictMode>
+    );
+    }
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
